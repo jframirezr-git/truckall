90a823f54ab660b8ac39888d479e5e1bcd193125
jframirezr-git <jframirezr@eafit.edu.co>
2025-11-30 13:45:15 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jframirezr-git.github.io/truckall',
   base: '/truckall/',
   vite: {
     plugins: [tailwindcss()]
